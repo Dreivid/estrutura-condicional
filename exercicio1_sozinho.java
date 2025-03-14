59281f3a421b4fe762378e5a0d20a9806e1fdc5a
Deivid Barreto <barreto.reis@hotmail.com>
2025-03-14 09:58:10 -0300

    Ler um código de um item e a quantidade deste item.

--- exercicio1_sozinho.java
+++ exercicio1_sozinho.java
@@ -7,19 +7,28 @@ public class exercicio1_sozinho {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
-		int HI = sc.nextInt();
-		int HF = sc.nextInt();
+		int codigo = sc.nextInt();
+		int quantidade = sc.nextInt();
 		
-		int duracao;
-				
-		if (HI < HF) {
-			duracao = HF - HI;
+		double total = 0;
+		
+		if (codigo == 1) {
+			total = quantidade * 4.0;
+		} 
+		else if (codigo == 2){
+			total =  quantidade * 4.5;
 		}
-		else {
-			duracao = 24 - HI + HF;
+		else if (codigo == 3) {
+			total = quantidade * 5.0;
+		}
+		else if (codigo == 4) {
+			total = quantidade * 2.0;
+		}
+		else if (codigo == 5) {
+			total = quantidade * 1.5;
 		}
 		
-		System.out.println("O JOGO DUROU " + duracao + "HORA(S)");
+		System.out.printf("Total: R$ %.2f%n", total);
 		
 		sc.close();
 		}
