5fdfdb897a90395952faedcc527910536a3186b8
Deivid Barreto <barreto.reis@hotmail.com>
2025-03-14 09:38:41 -0300

    Ler dois valores e ver se são multiplos

--- exercicio1_sozinho.java
+++ exercicio1_sozinho.java
@@ -7,17 +7,17 @@ public class exercicio1_sozinho {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		
-		int N = sc.nextInt();
+		int A = sc.nextInt();
+		int B = sc.nextInt();
 		
-		if (N % 2 == 0) {
-			System.out.println("PAR");
-		}
-		else {
-			System.out.println("IMPAR");
-		}
+		if (A % B == 0 || B % A == 0) {
+		System.out.println("Sao Multiplos ");
+		} else {
+			System.out.println("Nao sao Multiplos");
 		
-		sc.close();
-
+			
+			sc.close();
+		}
 	}
 
 }
